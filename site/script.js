489f691897ca5f81ff93ada2f07876d455531292
v5slv <98457679+v5slv@users.noreply.github.com>
2022-10-18 15:05:38 +0200

    vue glob axes d3

--- site/script.js
+++ site/script.js
@@ -1,128 +1,119 @@
+
+//Récupération des données du json
 fetch('moviedata.json')
   .then((response) => response.json())
   .then(function (moviedata) {
-      let labels = [...new Set(moviedata.map(movie => movie.year))];
-      console.log(labels);
 
-      let filmObjects = [];
-      let filmObject = new Object(moviedata.map(movie => filmObjects.push({
-        'titre':movie.title,
-        'x':movie.year,
-        'y':movie.rating,
-        'studio': movie.studio
-      })));
-      console.log(filmObjects)
-
-      let courbesStudio = [];
-      let studios = [...new Set(moviedata.map(movie => movie.studio))];
-      console.log(studios)
-      
-      let studioPush = new Set((studios.map(studio => 
-        courbesStudio.push({
-        'label': studio,
-      }))));
-      console.log(courbesStudio);
-      
-      //FAIRE AUTANT DE COURBES QUE DE STUDIOS
-      //DU COUP UN FOREACH DE STUDIO DANS UN TABLEAU STUDIOS
-      // un studio a plusieurs films, note des films = y, années des films = x
-
-      // une courbe a plusieurs films :
-      // dans son data il y aura [{film:"", x:2001, y:0},{film:"Shrek", x:2002, y:7,4}]
-
-      // const data = {
-      //   labels: labels,
-      //   let datasets = [];
-      //
-      // {
-      //       label: 'Pixar',
-      //       data: [0, 7.7, 7.4, 8.2, 7],
-      //       backgroundColor: 'rgb(255, 99, 132)',
-      //       borderColor: 'rgb(255, 99, 132)',
-      //     }
-      //   
-
-      const data = {
-        labels: labels,
-        datasets: [{
-            label: 'Pixar',
-            data: [{
-                x: 2001,
-                y: 0
-              },
-              {
-                x: 2002,
-                y: 5
-              },
-              {
-                x: 2003,
-                y: 8
-              },
-              {
-                x: 2004,
-                y: 7
-              },
-              {
-                x: 2005,
-                y: 7.5
-              }
-            ],
-            
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-          },
-          {
-            label: 'Disney',
-            data: [0, 7, 8, 8.7, 6.8],
-            backgroundColor: 'rgb(55, 132, 99)',
-            borderColor: 'rgb(55, 132, 99)',
-          },
-          {
-            label: 'Dreamworks',
-            data: [0, 6.8, 7, 7.2, 7.6],
-            backgroundColor: 'rgb(25, 99, 132)',
-            borderColor: 'rgb(25, 99, 132)',
-          },
-
-        ]
-      };
-
-      const config = {
-        type: 'line',
-        data: data,
-        options: {
-          responsive: true,
-          plugins: {
-            legend: {
-              position: 'right',
-            }
-          },
-          scales: {
-            y: {
-              max: 10,
-              min: 0,
-              ticks: {
-                stepSize: 1
-              }
-            }
-          },
-          elements: {
-            point: {
-              pointStyle: 'circle'
-            }
-          }
-        },
-      };
-
-
-      
-  
-
-      const chart = new Chart(
-        document.getElementById('chart'),
-        config
-      );
-
+    //Collecte des données à utiliser
+    let annees = [...new Set(moviedata.map(movie => movie.year))];
+    annees.push(2022, 2023);
+    console.log(annees);
+    
+    //------Fonction de couleur random à attribuer à chaque studio
+    function getRandomColor() {
+      var letters = '0123456789ABCDEF';
+      var color = '#';
+      for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
     }
 
-  )
+    //------Collecte des studios et ajout des couleurs dans objects
+    let studios = [...new Set(moviedata.map(movie => movie.studio))];
+    let datastudios = [];
+    let colorpush = new Object(studios.map(c => datastudios.push({
+      'studio': c,
+      'color': getRandomColor()
+    })));
+    console.log(datastudios);
+
+    //------Collecte des films et leurs données
+    let data = [];
+    let filmpush = new Object(moviedata.map(movie => data.push({
+      'titre': movie.title,
+      'year': movie.year,
+      'popularity': movie.rating,
+      'oscar': movie.oscar
+    })));
+    console.log(data);
+  
+  //Création du chart à vue générale
+  //------Création du svg dans la div, viewbox pour responsive
+  const svg = d3
+    .select("#chart")
+    .append("svg")
+    .attr("viewBox", "0 -10 600 310");
+  
+  //------Paramètres des margin
+  const strokeWidth = 1;
+  const margin = { top: 0, bottom: 20, left: 30, right: 20 };
+
+  //------Création du groupe svg pour faire le chart
+  const chart = svg
+    .append("g")
+    .attr("transform", `translate(${margin.left},0)`);
+  
+  //------Dimensions à utiliser pour délimiter les axes dans le chart
+  const width = 600 - margin.left - margin.right;
+  const height = 300 - margin.top - margin.bottom;
+
+  const grp = chart
+    .append("g")
+    .attr("transform", `translate(-${margin.left},-${margin.top})`);
+  
+  //Ajout des échelles des axes
+  const yScale = d3
+    .scaleLinear()
+    .range([height, 0])
+    .domain([5.5, 10]);
+    
+  const xScale = d3
+    .scaleLinear()
+    .range([0, width])
+    .domain(d3.extent(annees));
+
+  //Ajout de l'axe x
+  chart
+    .append("g")
+    .attr("transform", `translate(0,${height})`)
+    .call(d3.axisBottom(xScale).ticks(annees.length).tickFormat(d3.format("d")));
+  
+  //Ajout de l'axe y
+  chart
+    .append("g")
+    .attr("transform", `translate(0, 0)`)
+    .call(d3.axisLeft(yScale));
+  
+    
+
+  //Ajout barres de données
+  grp
+  .append("rect")
+  .attr("transform",`translate(${margin.left},0)`)
+  .datum(data)
+  .style("fill", "lightblue")
+  .attr("width", d.rating - 5.5)
+  .attr("height", )
+
+    //PARTIE HS POUR UN CHART AREA
+//   const area = d3
+//     .area()
+//     .x(d => xScale(d.year))
+//     .y0(height)
+//     .y1(d => yScale(d.popularity));
+
+      // Add area
+//   grp
+//     .append("path")
+//     .attr("transform", `translate(${margin.left},0)`)
+//     .datum(data)
+//     .style("fill", "lightblue")
+//     .attr("stroke", "steelblue")
+//     .attr("stroke-linejoin", "round")
+//     .attr("stroke-linecap", "round")
+//     .attr("stroke-width", strokeWidth)
+//     .attr("d", area);
+  
+
+})
